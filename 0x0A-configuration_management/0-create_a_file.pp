55b054b977e65fcafb123e68d51ea5894447fa13
peterGeswint <petergeswint@gmail.com>
2024-07-18 13:30:32 +0200

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# creates a file
+# creates a file in /tmp using puppet
+
 file {'/tmp/school':
-	mode	=> '0744',
-	owner	=> 'www-data',
-	group	=> 'www-data',
-	content	=> 'I love puppet'
-
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
-
